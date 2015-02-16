dd4060d11d381b88fc6f6e7b233b29e9ed513684
juanramirez99 <guiller956@gmail.com>
2015-02-15 20:46:45 -0500

    before add crud

--- app/src/main/java/com/example/user/contactsapp/AddContactActivity.java
+++ app/src/main/java/com/example/user/contactsapp/AddContactActivity.java
@@ -4,20 +4,28 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.j256.ormlite.android.apptools.OpenHelperManager;
+import com.j256.ormlite.dao.Dao;
+
+import java.sql.SQLException;
+
 
 public class AddContactActivity extends ActionBarActivity {
 
     public final static String FIRSTNAME = "FIRSTNAME";
     public final static String LASTNAME  = "LASTNAME";
+    private final static String LOG_TAG = AddContactActivity.class.getSimpleName();
     private EditText mFirstName;
     private EditText mLastName;
     private Button mDone;
+    DataBaseHelper mDBHelper = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,13 +39,32 @@ public class AddContactActivity extends ActionBarActivity {
         mDone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                ContactSingle contact = new ContactSingle();
+                contact.setLastName(mFirstName.getText().toString());
+                contact.setFirstName(mLastName.getText().toString());
+                SaveContact(contact);
                 Intent mainActivity = new Intent();
-                mainActivity.putExtra(FIRSTNAME,mFirstName.getText().toString());
-                mainActivity.putExtra(LASTNAME ,mLastName.getText().toString());
+                mainActivity.putExtra(FIRSTNAME,contact.getFirstName());
+                mainActivity.putExtra(LASTNAME ,contact.getLastName());
                 setResult(Activity.RESULT_OK, mainActivity);
                 finish();
             }
         });
+
+    }
+    private void SaveContact(ContactSingle contact) {
+        try {
+            Dao<ContactSingle,Integer> dao = getDBHelper().getContactDao();
+            dao.create(contact);
+        } catch (SQLException e) {
+            Log.e(LOG_TAG, "failed to create dao");
+        }
+    }
+
+    public DataBaseHelper getDBHelper() {
+        if (mDBHelper == null)
+            mDBHelper= OpenHelperManager.getHelper(this, DataBaseHelper.class);
+        return mDBHelper;
 
     }
 
@@ -62,5 +89,14 @@ public class AddContactActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onDestroy() {
+        if(mDBHelper != null){
+            OpenHelperManager.releaseHelper();
+            mDBHelper = null;
+        }
+        super.onDestroy();
     }
 }
